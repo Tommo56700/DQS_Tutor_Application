2ee62a6cc9a73b9a9a52a6cf7c8d8ef6e27183d0
Tommo56700 <sean.wtomlinson@talktalk.net>
2017-03-23 13:05:33 +0000

    Updated (but broken)

--- SearchStudents.py
+++ SearchStudents.py
@@ -1,13 +1,6 @@
 #2.Searching the personal tutor list for individual students 
 from tkinter import *
-
-
-testTutor = Tutor(12, "CM101", "Hailey", "Smiths", "hsmiths@uni.co.uk", True, 10)
-testStudent = Student(1, "CM101", "Fred", "Jones", testTutor, "2014/15", "fjones@uni.co.uk")
-testStudent2 = Student(2, "CM101", "Fred", "Jones", testTutor, "2014/15", "fjones@uni.co.uk")
-Students = [testStudent, testStudent2]
-testTutor.addStudent(testStudent)
-testTutor.addStudent(testStudent2)
+from NewStudents import *
 
 bufferCounter = 0
 
@@ -34,7 +27,7 @@ class SearchStudents(Frame):
 		entID = Entry(self, font=("Helvetica", 10))
 		entID.grid(row = 1, column = 0)
 
-		butSearch = Button(self, text="Search", font=("Helvetica", 10), command = lambda: SearchByID(int(entID.get())))
+		butSearch = Button(self, text="Search", font=("Helvetica", 10), command = lambda: SearchByID(entID.get()))
 		butSearch.grid(row=1, column=1, pady=(0, 0))
 
 		butQuit = Button(self, text="Quit", font=("Helvetica", 10), command=self.quit)
